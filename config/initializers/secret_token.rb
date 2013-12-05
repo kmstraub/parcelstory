c22a07392ba137460c9c94500dc435468a15c185
kmstraub <kmstraub@gmail.com>
2013-12-05 14:05:33 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Parcelstory::Application.config.secret_key_base = '944a4ebe2fc2d744b99ce4a33817f3bb6f159fe7fb374d525da9c8e7502f46f785a8db8fd3b3209afe051cbcd30175367c6a41b33dcf97841d30937a5296b372'
